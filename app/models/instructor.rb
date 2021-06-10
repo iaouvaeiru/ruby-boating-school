7846b08517667802d2572b9345a40570175957c5
Leon Fan <leonkind@gmail.com>
2021-06-10 16:43:52 -0500

    instructor#pass_student doesnt quite work

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,40 @@
 class Instructor
-
+    @@all = []
+    attr_reader :name
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def all_tests
+        BoatingTest.all.select {|test| test.instructor == self}
+    end
+    def passed_tests
+        all_tests.select {|test| test.test_status == "passed"}
+    end
+    def passed_students
+        passed_tests.map {|test| test.student}
+    end
+    def pass_student(student, test)
+        test_to_update = BoatingTest.all.find{|test| test.test_name == test && test.student == student}
+        binding.pry
+        if test_to_update.nil? 
+            BoatingTest.new(student, test, "passed", self)
+        else
+            test_to_update.test_status = "passed"
+        end
+    end
+    def fail_student(student, test)
+        test_to_update = BoatingTest.all.find{|test| test.test_name == test && test.student == student}
+        if test_to_update.nil? 
+            BoatingTest.new(student, test, "failed", self)
+        else
+            test_to_update.test_status = "failed"
+        end
+    end
+    def all_students
+        all_tests.map {|test| test.student}
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,29 @@
 class Student
-
+    @@all = []
+    attr_reader :first_name
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def add_boating_test(name, status, instructor)
+        BoatingTest.new(self, name, status, instructor)
+    end
+    def all_tests
+        BoatingTest.all.select {|test| test.student == self}
+    end
+    def all_instructors
+        all_tests.map {|test| test.instructor}
+    end
+    def self.find_student(name)
+        Student.all.find {|student| student.first_name == name}
+    end
+    def passed_tests
+        all_tests.select {|test| test.test_status == "passed"}
+    end
+    def grade_percentage
+        (passed_tests.count.to_f / all_tests.count)*100
+    end
 end
